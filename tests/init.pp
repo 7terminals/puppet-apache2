b405d5ae07d371c736fe25139f7b01e4b309f409
Francis Pereira <francispereira@7terminals.com>
2012-08-10 11:31:31 +0530

    fixed syntax errors

--- tests/init.pp
+++ tests/init.pp
@@ -1,8 +1,7 @@
-include apache2
-apache2::virtualhost {"example.com":
-	$subdomains => "www.example.com",
-	$php_admin_value = undef,
-	$domain_id = 4000,
-	$password = 'test'
-	
-	}
+include apache2 apache2::virtualhost {
+	"example.com" :
+		subdomains => "www.example.com",
+		php_admin_value => undef,
+		domain_id => 4000,
+		password => 'test',
+}
